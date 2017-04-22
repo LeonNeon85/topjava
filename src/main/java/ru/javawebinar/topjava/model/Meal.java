2a45c557bbb5496d7a1ec40b7ea2aa28f4e557dc
New Name <chernigov_86@ukr.net>
2017-04-22 12:43:18 +0300

    HW04 - base (not ready)

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -10,14 +10,36 @@ import java.time.LocalTime;
  * GKislin
  * 11.01.2015.
  */
+
+
+   // LEFT JOIN FETCH u.roles
+
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.BEETWENE, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime desc"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m LEFT JOIN FETCH m.user WHERE m.id=:user_id ORDER BY m.dateTime desc "),
+})
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")} )
 public class Meal extends BaseEntity {
+
+    public static final String DELETE = "Meal.DELETE";
+    public static final String BEETWENE = "Meal.BEETWENE";
+    public static final String ALL_SORTED = "Meal.ALL_SORTED";
+
+    @NotNull
+    @Column(name = "date_time", columnDefinition = "timestamp default now()", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
     private String description;
 
+    @Column(name = "calories", nullable = false, columnDefinition = "int default 1000")
     private int calories;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
